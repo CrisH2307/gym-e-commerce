aed2465f10be5b820ddee601145b218d01c2fb9c
CrisH2307 <khangpicasso@gmail.com>
2023-12-20 15:16:13 -0500

    Update master

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <div>
-        Hello
-      </div>
+      <div>Hello HEllo</div>
     </>
-  )
+  );
 }
