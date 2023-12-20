74ac213dff9327935c29b7b4935293e0c70bc305
CrisH2307 <khangpicasso@gmail.com>
2023-12-20 15:33:37 -0500

    Add Components

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import Header from "@/components/header/Header";
+import Navbar from "@/components/navbar/Navbar";
 
 export default function Home() {
   return (
-    <>
-      <div>Hello HEllo</div>
-    </>
+    <div>
+      <Header />
+      <Navbar />
+    </div>
   );
 }
