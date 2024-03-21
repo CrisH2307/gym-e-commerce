5624726e405aa6589e0a8493f774e725ce494fae
Phan Trung Kien <kevinkien2706@gmail.com>
2024-03-21 00:32:12 -0400

    Update the SearchBar Component
    SearchBar

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import Data from "../../components/Assets/data";
+import "./search.css";
+import BackspaceIcon from "@mui/icons-material/Backspace";
+const Search = () => {
+  const [query, setQuery] = useState("");
+  const inputRef = useRef(null);
 
-export default function Search() {
+  const clearSearch = () => {
+    setQuery("");
+    if (inputRef.current) {
+      inputRef.current.value = "";
+    }
+  };
   return (
-    <div>
-      <h1>Search Bar</h1>
+    <div className="body">
+      <div className="searchInputs">
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="Search..."
+          className="search"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        {query.length > 0 && (
+          <div className="searchIcon" onClick={clearSearch}>
+            <BackspaceIcon />
+          </div>
+        )}
+      </div>
+      <span>Suggestions</span>
+      <ul className="list">
+        {Data.filter((data) => data.name.toLowerCase().includes(query)).map(
+          (data_product) => (
+            <li key={data_product.id} className="listItem">
+              {data_product.name}
+            </li>
+          )
+        )}
+      </ul>
     </div>
   );
-}
+};
+
+export default Search;
